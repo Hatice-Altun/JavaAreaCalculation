96688c10a7b9035acd247fd2009bb4c6dd3e8597
Hatice-Altun <van.altun@outlook.com>
2022-04-20 19:00:43 +0200

    area calculation

---
+++ src/novi/basics/Main.java
@@ -0,0 +1,15 @@
+package novi.basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Area Calculation
+
+        double roomWidth = 5.55;
+        double roomLength = 4.5;
+        double roomArea = roomWidth * roomLength;
+
+        System.out.println(roomArea);
+
+    }
+}
